90b35f69c02fd2e5abfa4255912775c259ab4708
Syed Khurshid <syedkhurshid@Syeds-MBP.lan>
2021-01-11 17:20:25 -0600

    done the front side

--- front/src/Pages/Payment/Card.jsx
+++ front/src/Pages/Payment/Card.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect } from "react";
-import { Elements, CardElement } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-const dotenv = require("dotenv").config();
-import { Button } from "@material-ui/core";
+import { CardElement } from "@stripe/react-stripe-js";
 
 //Make sure to call `loadStripe` outside of a component's renter to avoid
 // recreating the `Stripe` object on every render
-const key = process.env.Secret_Key;
+
 // const stripePromise = loadStripe(key.toString());
 
 export default function Card() {
   return (
     <div>
-      <Button variant="contained" color="primary">
-        Pay Now
-      </Button>
+      <CardElement
+        options={{
+          hidePostalCode: true,
+          style: {
+            base: { fontSize: "23px" },
+          },
+        }}
+      />
     </div>
   );
 }
--- front/src/Pages/Payment/CheckOut.jsx
+++ front/src/Pages/Payment/CheckOut.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import dotenv from "dotenv";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 import {
   Grid,
@@ -15,11 +17,11 @@ import {
   TextField,
   FormControlLabel,
   Checkbox,
+  Button,
 } from "@material-ui/core";
 import Card from "./Card";
 
-dotenv.config({ path: "../../.env" });
-
+dotenv.config();
 //for Styles
 const useStyles = makeStyles({
   roothead: {
@@ -30,6 +32,8 @@ const useStyles = makeStyles({
     marginTop: "1rem",
   },
 });
+
+const stripePromise = loadStripe(process.env.REACT_APP_Stripe_PUBLISHIBLE_KEY);
 
 export default function CheckOut() {
   const classes = useStyles();
@@ -100,7 +104,7 @@ export default function CheckOut() {
 
   useEffect(() => {
     UpdateData();
-    console.log(process.env);
+    console.log(process.env.REACT_APP_Stripe_PUBLISHIBLE_KEY);
   }, [data]);
 
   useEffect(() => {
@@ -109,250 +113,266 @@ export default function CheckOut() {
 
   return (
     <Grid container spacing={3} align="center" className={classes.roothead}>
-      <Grid item sm={6} xs={12}>
-        <TableContainer
-          component={Paper}
-          style={{
-            width: "100%",
-            marginTop: "1.2rem",
-            paddingTop: "10px",
-            paddingBottom: "10px",
-          }}>
-          <TableHead>
-            <TableRow>
-              <TableCell style={{ fontSize: "2rem" }} align="left">
-                Detail
-              </TableCell>
-              <TableCell style={{ fontSize: "2rem" }} align="right">
-                $USD
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell style={{ fontSize: "1rem" }} align="left">
-                Total Before Tax
-              </TableCell>
-              <TableCell
-                style={{ fontSize: "1rem", fontWeight: "100" }}
-                align="right">
-                {formatter.format(
-                  data
-                    .map((x) => x.Cost)
-                    .reduce((acc, curr) => {
-                      return acc + curr;
-                    }, 0)
-                )}
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell style={{ fontSize: "1rem" }} align="left">
-                Tax Percentage
-              </TableCell>
-              <TableCell
-                style={{ fontSize: "1rem", fontWeight: "100" }}
-                align="right">
-                {percentageTax}
-              </TableCell>
-            </TableRow>
+      <Elements stripe={stripePromise}>
+        <Grid item sm={6} xs={12}>
+          <TableContainer
+            component={Paper}
+            style={{
+              width: "100%",
+              marginTop: "1.2rem",
+              paddingTop: "10px",
+              paddingBottom: "10px",
+            }}>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ fontSize: "2rem" }} align="left">
+                  Detail
+                </TableCell>
+                <TableCell style={{ fontSize: "2rem" }} align="right">
+                  $USD
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell style={{ fontSize: "1rem" }} align="left">
+                  Total Before Tax
+                </TableCell>
+                <TableCell
+                  style={{ fontSize: "1rem", fontWeight: "100" }}
+                  align="right">
+                  {formatter.format(
+                    data
+                      .map((x) => x.Cost)
+                      .reduce((acc, curr) => {
+                        return acc + curr;
+                      }, 0)
+                  )}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontSize: "1rem" }} align="left">
+                  Tax Percentage
+                </TableCell>
+                <TableCell
+                  style={{ fontSize: "1rem", fontWeight: "100" }}
+                  align="right">
+                  {percentageTax}
+                </TableCell>
+              </TableRow>
 
-            <TableRow>
-              <TableCell style={{ fontSize: "1rem" }} align="left">
-                Tax
-              </TableCell>
-              <TableCell
-                style={{ fontSize: "1rem", fontWeight: "100" }}
-                align="right">
-                {" "}
-                {formatter.format(
-                  data
-                    .map((x) => x.Cost)
-                    .reduce((accum, currentValue) => {
-                      return accum + currentValue;
-                    }, 0) * taxRate
-                )}
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell style={{ fontSize: "2rem" }} align="left">
-                Total
-              </TableCell>
-              <TableCell style={{ fontSize: "2rem" }} align="right">
-                {formatter.format(
-                  data
-                    .map((x) => x.Cost)
-                    .reduce((accum, currentValue) => {
-                      return accum + currentValue;
-                    }, 0) *
-                    (taxRate + 1)
-                )}
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </TableContainer>
-      </Grid>
-      <Grid item sm={6} xs={12}>
-        <Paper style={{ marginTop: "1.2rem", padding: "1rem" }}>
-          <div>
-            <Typography variant="h4" align="left" style={{ padding: "10px" }}>
-              Shipping Address
-            </Typography>
-          </div>
+              <TableRow>
+                <TableCell style={{ fontSize: "1rem" }} align="left">
+                  Tax
+                </TableCell>
+                <TableCell
+                  style={{ fontSize: "1rem", fontWeight: "100" }}
+                  align="right">
+                  {" "}
+                  {formatter.format(
+                    data
+                      .map((x) => x.Cost)
+                      .reduce((accum, currentValue) => {
+                        return accum + currentValue;
+                      }, 0) * taxRate
+                  )}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontSize: "2rem" }} align="left">
+                  Total
+                </TableCell>
+                <TableCell style={{ fontSize: "2rem" }} align="right">
+                  {formatter.format(
+                    data
+                      .map((x) => x.Cost)
+                      .reduce((accum, currentValue) => {
+                        return accum + currentValue;
+                      }, 0) *
+                      (taxRate + 1)
+                  )}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </TableContainer>
+        </Grid>
+        <Grid item sm={6} xs={12}>
+          <Paper style={{ marginTop: "1.2rem", padding: "1rem" }}>
+            <div>
+              <Typography variant="h4" align="left" style={{ padding: "10px" }}>
+                Shipping Address
+              </Typography>
+            </div>
 
-          <form>
-            <TextField
-              required
-              id="outlined-required"
-              label="Name"
-              type="text"
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="Name"
-              onChange={Address}
-            />
-            <TextField
-              required
-              id="outlined-full-width"
-              label="Street Address"
-              type="text"
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="StreetAddress"
-              onChange={Address}
-            />
-            <TextField
-              id="outlined-full-width"
-              label="Apt#"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              onChange={Address}
-              name="Apt"
-            />
-            <TextField
-              id="outlined-full-width"
-              type="number"
-              label="Zip Code"
-              variant="outlined"
-              margin="normal"
-              name="Zipcode"
-              onChange={Address}
-            />
-            <TextField
-              id="outlined-margin-none"
-              type="text"
-              label="State"
-              variant="outlined"
-              margin="normal"
-              style={{ width: "15ch", marginLeft: "1rem" }}
-              onChange={Address}
-              name="State"
-            />
-            <TextField
-              id="outlined-margin-none"
-              type="text"
-              label="Country"
-              placeholder="United States"
-              variant="outlined"
-              margin="normal"
-              style={{ width: "19.8ch", marginLeft: "1rem" }}
-              onChange={Address}
-              name="Country"
-            />
-          </form>
-        </Paper>
-        <Paper style={{ marginTop: "1.2rem", padding: "1rem" }}>
-          <div>
-            <Typography variant="h4" align="left" style={{ padding: "10px" }}>
-              Billing Address
-            </Typography>
-          </div>
-          <div>
-            <FormControlLabel
-              control={<Checkbox name="checkedA" onChange={SelectCheckbox} />}
-              label="Same as the Shipping Address"
-            />
-          </div>
+            <form>
+              <TextField
+                required
+                id="outlined-required"
+                label="Name"
+                type="text"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="Name"
+                onChange={Address}
+              />
+              <TextField
+                required
+                id="outlined-full-width"
+                label="Street Address"
+                type="text"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="StreetAddress"
+                onChange={Address}
+              />
+              <TextField
+                id="outlined-full-width"
+                label="Apt#"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                onChange={Address}
+                name="Apt"
+              />
+              <TextField
+                id="outlined-full-width"
+                type="number"
+                label="Zip Code"
+                variant="outlined"
+                margin="normal"
+                name="Zipcode"
+                onChange={Address}
+              />
+              <TextField
+                id="outlined-margin-none"
+                type="text"
+                label="State"
+                variant="outlined"
+                margin="normal"
+                style={{ width: "15ch", marginLeft: "1rem" }}
+                onChange={Address}
+                name="State"
+              />
+              <TextField
+                id="outlined-margin-none"
+                type="text"
+                label="Country"
+                placeholder="United States"
+                variant="outlined"
+                margin="normal"
+                style={{ width: "19.8ch", marginLeft: "1rem" }}
+                onChange={Address}
+                name="Country"
+              />
+            </form>
+          </Paper>
+          <Paper style={{ marginTop: "1.2rem", padding: "1rem" }}>
+            <div>
+              <Typography variant="h4" align="left" style={{ padding: "10px" }}>
+                Billing Address
+              </Typography>
+            </div>
+            <div>
+              <FormControlLabel
+                control={<Checkbox name="checkedA" onChange={SelectCheckbox} />}
+                label="Same as the Shipping Address"
+              />
+            </div>
 
-          <form>
-            <TextField
-              required
-              id="outlined-required"
-              label="Name"
-              type="text"
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              value={!check ? billingAddress.Name : ChangeBillingAddress}
-              onChange={ChangeBillingAddress}
-            />
-            <TextField
-              required
-              id="outlined-full-width"
-              label="Street Address"
-              type="text"
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              value={
-                !check ? billingAddress.StreetAddress : ChangeBillingAddress
-              }
-              onChange={ChangeBillingAddress}
-            />
-            <TextField
-              id="outlined-full-width"
-              label="Apt#"
-              variant="outlined"
-              fullWidth
-              name="Apt"
-              margin="normal"
-              value={!check ? billingAddress.Apt : ChangeBillingAddress}
-              onChange={ChangeBillingAddress}
-            />
-            <TextField
-              id="outlined-full-width"
-              type="number"
-              label="Zip Code"
-              variant="outlined"
-              margin="normal"
-              name="Zipcode"
-              value={!check ? billingAddress.Zipcode : ChangeBillingAddress}
-              onChange={ChangeBillingAddress}
-            />
-            <TextField
-              id="outlined-margin-none"
-              type="text"
-              label="State"
-              variant="outlined"
-              margin="normal"
-              style={{ width: "15ch", marginLeft: "1rem" }}
-              name="State"
-              value={!check ? billingAddress.State : ChangeBillingAddress}
-              onChange={ChangeBillingAddress}
-            />
-            <TextField
-              id="outlined-margin-none"
-              type="text"
-              label="Country"
-              placeholder="United States"
-              variant="outlined"
-              margin="normal"
-              style={{ width: "19.8ch", marginLeft: "1rem" }}
-              name="Country"
-              value={!check ? billingAddress.Country : ChangeBillingAddress}
-              onChange={ChangeBillingAddress}
-            />
-          </form>
-        </Paper>
-        <Paper>
-          <Card
-            style={{ width: "100%", marginTop: "1.2rem" }}
-            onClick={PaymentProcess}
-          />
-        </Paper>
-      </Grid>
+            <form>
+              <TextField
+                required
+                id="outlined-required"
+                label="Name"
+                type="text"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                value={!check ? billingAddress.Name : ChangeBillingAddress}
+                onChange={ChangeBillingAddress}
+              />
+              <TextField
+                required
+                id="outlined-full-width"
+                label="Street Address"
+                type="text"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                value={
+                  !check ? billingAddress.StreetAddress : ChangeBillingAddress
+                }
+                onChange={ChangeBillingAddress}
+              />
+              <TextField
+                id="outlined-full-width"
+                label="Apt#"
+                variant="outlined"
+                fullWidth
+                name="Apt"
+                margin="normal"
+                value={!check ? billingAddress.Apt : ChangeBillingAddress}
+                onChange={ChangeBillingAddress}
+              />
+              <TextField
+                id="outlined-full-width"
+                type="number"
+                label="Zip Code"
+                variant="outlined"
+                margin="normal"
+                name="Zipcode"
+                value={!check ? billingAddress.Zipcode : ChangeBillingAddress}
+                onChange={ChangeBillingAddress}
+              />
+              <TextField
+                id="outlined-margin-none"
+                type="text"
+                label="State"
+                variant="outlined"
+                margin="normal"
+                style={{ width: "15ch", marginLeft: "1rem" }}
+                name="State"
+                value={!check ? billingAddress.State : ChangeBillingAddress}
+                onChange={ChangeBillingAddress}
+              />
+              <TextField
+                id="outlined-margin-none"
+                type="text"
+                label="Country"
+                placeholder="United States"
+                variant="outlined"
+                margin="normal"
+                style={{
+                  width: "19.8ch",
+                  marginLeft: "1rem",
+                }}
+                name="Country"
+                value={!check ? billingAddress.Country : ChangeBillingAddress}
+                onChange={ChangeBillingAddress}
+              />
+            </form>
+          </Paper>
+          <Paper
+            style={{
+              height: "2rem",
+              marginTop: "1rem",
+              marginBottom: "1.2rem",
+
+              alignItems: "center",
+              justifyItems: "center",
+              alignContent: "center",
+              justifyContent: "center",
+            }}>
+            <Card onClick={PaymentProcess} />
+          </Paper>
+
+          <Button variant="contained" color="primary">
+            Pay Now
+          </Button>
+        </Grid>
+      </Elements>
     </Grid>
   );
 }
