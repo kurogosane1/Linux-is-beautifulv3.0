5eb30585812c42f27b5dfc6292780a435b306c1a
Syed Khurshid <magnite@hotmail.com>
2021-01-30 20:51:08 -0600

    made some changes to the connection

--- Controller/Connection.js
+++ Controller/Connection.js
@@ -4,9 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_DATABASE,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
