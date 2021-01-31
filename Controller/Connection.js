33f95612c18bb1f513cc002db7f168a4f069a51c
Syed Khurshid <magnite@hotmail.com>
2021-01-30 20:30:59 -0600

    modified something

--- Controller/Connection.js
+++ Controller/Connection.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   }
 );
