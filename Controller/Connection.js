2e5549277879f986da108849976e5ec9740a43df
Syed Khurshid <magnite@hotmail.com>
2021-01-30 20:58:55 -0600

    another test

--- Controller/Connection.js
+++ Controller/Connection.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   // process.env.DB_USER,
   // process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    JAWSDB_URL: process.env.DB_HOST,
     dialect: "mysql",
   }
 );
