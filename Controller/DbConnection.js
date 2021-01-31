f3e29356dc77dd68cc04c7bafc7e9c5f2e445c09
Syed Khurshid <magnite@hotmail.com>
2021-01-30 19:58:28 -0600

    modified connection

--- Controller/DbConnection.js
+++ Controller/DbConnection.js
@@ -1,7 +1,9 @@
 const redis = require("redis");
 
 //Configure Redis
-const redisClient = redis.createClient({ port: 6379, host: "localhost" });
-
+// const redisClient = redis.createClient({ port: 6379, host: "localhost" });
+const redisClient = redis.createClient(process.env.REDISCLOUD_URL, {
+  no_ready_check: true,
+});
 
 module.exports = redisClient;
