1d4441829ff151ec32c30d4d6ec239443237562c
Syed Khurshid <magnite@hotmail.com>
2021-01-31 18:37:13 -0600

    Made some changes to checkout

--- front/src/Pages/Payment/Card.jsx
+++ front/src/Pages/Payment/Card.jsx
@@ -4,11 +4,7 @@ import { CardElement } from "@stripe/react-stripe-js";
 export default function Card() {
   return (
     <div>
-      <div>
-        <p>Please use 4242 4242 4242 4242 for card number</p>
-        <p>security as 222 and expiry date as 02/22</p>
-        <p>No Charge will be placed to you</p>
-      </div>
+      
       <CardElement
         options={{
           hidePostalCode: true,
--- front/src/Pages/Payment/CheckOut.jsx
+++ front/src/Pages/Payment/CheckOut.jsx
@@ -472,6 +472,11 @@ export default function CheckOut(props) {
               value={!check ? billingAddress.Email : ChangeBillingAddress}
               onChange={ChangeBillingAddress}
             />
+            <div>
+              <p>Please use 4242 4242 4242 4242 for card number</p>
+              <p>security as 222 and expiry date as 02/22</p>
+              <p>No Charge will be placed to you</p>
+            </div>
           </Paper>
           <Paper
             style={{
@@ -483,7 +488,6 @@ export default function CheckOut(props) {
               alignContent: "center",
               justifyContent: "center",
             }}>
-            
             <Card />
           </Paper>
 
