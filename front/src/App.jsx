33ac277ca1cc153b1b89785b3b52b835ee424b2c
Syed Khurshid <magnite@hotmail.com>
2021-05-08 19:54:19 -0500

    made some small changes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -37,9 +37,9 @@ function App() {
 
   return (
     <>
-      <ScrollToTop />
       <Router>
         <div className="App">
+          <ScrollToTop />
           <Nav />
           <Container maxWidth="lg">
             <Switch>
